d8403d169d717a14472880670f4c0fd2bed1725f
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-04-12 22:50:17 -0300

    initial setup

---
+++ app/src/main/java/tech/henriquedev/memorynotes/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package tech.henriquedev.memorynotes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.henriquedev.memorynotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
