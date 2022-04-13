38de084e4a29ccae724cf133a50cf98ccdb5ef6e
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-04-12 23:39:18 -0300

    implemented repository data source

---
+++ core/src/main/java/tech/henriquedev/core/repository/NoteRepository.kt
@@ -0,0 +1,13 @@
+package tech.henriquedev.core.repository
+
+import tech.henriquedev.core.data.Note
+
+class NoteRepository(private val dataSource: NoteDataSource) {
+    suspend fun addNote(note: Note) = dataSource.add(note)
+
+    suspend fun getNote(id: Long) = dataSource.get(id)
+
+    suspend fun getAllNotes() = dataSource.getAll()
+
+    suspend fun removeNote(note: Note) = dataSource.remove(note)
+}
