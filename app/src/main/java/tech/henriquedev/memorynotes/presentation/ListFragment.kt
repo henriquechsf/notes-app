fd536df671654383d4d3c8bfbcc4185a1c5d1d72
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-04-13 22:56:00 -0300

    fragments

--- app/src/main/java/tech/henriquedev/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/tech/henriquedev/memorynotes/presentation/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import tech.henriquedev.memorynotes.R
 
 class ListFragment : Fragment() {
@@ -15,4 +16,13 @@ class ListFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+//    private fun goToNoteDetails(id: Long = 0L) {
+//        val action = ListFragmentDirections.actionGoToNote(id)
+//        Navigation.findNavController(notesListView).navigate(action)
+//    }
 }
